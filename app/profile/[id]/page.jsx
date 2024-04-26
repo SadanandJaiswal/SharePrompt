a8dadc3fb84e57b55cd882157944b1767eb90ec8
Sadanand Jaiswal <sadanandjaiswal92@gmail.com>
2024-04-26 22:53:12 +0530

    trying to solve useSearchParams error occuring while deploying to vercel

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -1,15 +1,17 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense  } from "react";
 import { useSearchParams } from "next/navigation";
 
 import Profile from "@components/Profile";
+import Loading from "../loading";
 
 const UserProfile = ({ params }) => {
   const searchParams = useSearchParams();
-  const userName = searchParams.get("name");
+  // const userName = searchParams.get("name");
 
   const [userPosts, setUserPosts] = useState([]);
+  const [userName, setUserName] = useState(null);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -17,17 +19,20 @@ const UserProfile = ({ params }) => {
       const data = await response.json();
 
       setUserPosts(data);
+      setUserName(()=> searchParams.get("name"));
     };
 
     if (params?.id) fetchPosts();
   }, [params.id]);
 
   return (
-    <Profile
-      name={userName}
-      desc={`Welcome to ${userName}'s personalized profile page. Explore ${userName}'s exceptional prompts and be inspired by the power of their imagination`}
-      data={userPosts}
-    />
+    <Suspense fallback={<Loading />}>
+        <Profile
+          name={userName}
+          desc={`Welcome to ${userName}'s personalized profile page. Explore ${userName}'s exceptional prompts and be inspired by the power of their imagination`}
+          data={userPosts}
+        />
+    </Suspense>
   );
 };
 
